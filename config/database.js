6867dffa52071849db20dae68f27fb501cf9c8c2
Carlos Alexandre das Dores <olletecnologia@gmail.com>
2020-07-09 14:12:09 -0300

    Ambiente de Dev

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 module.exports = {
-  username: 'root',
-  password: '0cccc2a3',
-  database: 'sbi_prod',
-  host: '35.185.114.41',
-  dialect: 'mysql',
-  define:{
-    timestamps: false,
-    underscored: true,
-    freezeTableName: true,
-  },
+    username: 'root',
+    password: '0cccc2a3',
+    database: 'sbi_dev',
+    host: '35.185.114.41',
+    dialect: 'mysql',
+    define: {
+        timestamps: false,
+        underscored: true,
+        freezeTableName: true,
+    },
 }
